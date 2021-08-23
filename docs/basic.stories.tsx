8a3cd29cdfc668a09a3ed45eb30d67b7943879b9
wwsun <ww.sun@outlook.com>
2021-08-23 15:55:26 +0800

    docs: add stories

---
+++ docs/basic.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from 'coral';
+
+export default {
+  title: 'Basic',
+};
+
+export function Basic() {
+  return (<Box p="m" bg="gray.10" color="primary.60">
+    hello world
+  </Box>)
+}
+
