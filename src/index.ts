5caaeb60486d438695fb1b759d6b5532316b6691
wwsun <ww.sun@outlook.com>
2022-01-11 19:27:04 +0800

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './theme';
 export * from './components';
 export * from './provider';
 export * from './forwad-ref';
+export type { SystemProps, CoralProps, HTMLCoralProps } from './types';
