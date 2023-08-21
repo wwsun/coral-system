3223d5fc9e61d88ad01e5fba56545c37c1c15ba7
wwsun <ww.sun@outlook.com>
2023-08-21 17:46:16 +0800

    fix: export styled

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export * from './theme';
 export * from './components';
 export * from './provider';
 export * from './forward-ref';
-export { css, keyframes } from 'styled-components';
+export { default as styled, css, keyframes } from 'styled-components';
 export type { SystemProps, CoralProps, HTMLCoralProps } from './types';
